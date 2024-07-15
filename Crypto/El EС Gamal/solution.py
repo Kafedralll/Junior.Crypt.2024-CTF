5fdd3f16bd402cc7e5d9513c1cd4dcf67f6ff0a3
Kafedralll <167564641+Kafedralll@users.noreply.github.com>
2024-07-15 23:40:23 +0300

    Update solution.py

--- Crypto/El EС Gamal/solution.py
+++ Crypto/El EС Gamal/solution.py
@@ -1,1 +1,14 @@
+sage: e = EllipticCurve(GF(3547),[1801,461])
+sage: e
+Elliptic Curve defined by y^2 = x^3 + 1801*x + 461 over Finite Field of size 3547
+sage: P = e(1688,2431)
+sage: Q = e(2742,1260)
+sage: for k in range(0, len(e.points())):
+....:     if Q == k*P:
+....:         print (k)
+2302
+sage: k = 2302
+sage: Q == k*P
+True
+sage:
 
